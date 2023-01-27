70a3e0d0a317e2953dfe741d3dc8ab8968bd79c0
Martin C <martin.couture@uqtr.ca>
2023-01-26 21:36:51 -0500

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  variableInputDeParent:string =""
+  inputParent:string = ""
   title = 'sem3';
+  valeurTexteEnfant:string = ""
+
+  envoyerVenfant(){
+
+    this.variableInputDeParent = this.inputParent
+  }
+
+  afficheDeLenfant(message:string){
+    this.valeurTexteEnfant = message
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnfantComponent } from './enfant/enfant.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnfantComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
